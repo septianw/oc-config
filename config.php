7a53293bd1748b7e45f517dd5152f8fcac4bfb95
Septian Wibisono <asep.gelo@gmail.com>
2012-10-07 23:01:33 +0700

    first init

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DEBUG", true);
+
+$CONFIG = array(
+/* Flag to indicate ownCloud is successfully installed (true = installed) */
+"installed" => false,
+"datadirectory" => "/var/www/data",
+"appstoreenabled" => false
+);
