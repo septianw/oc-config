a59fb98856021af667407a8483f389ecb96ee5e6
Septian Wibisono <asep.gelo@gmail.com>
2012-10-08 04:43:13 +0700

    mount ftp added

---
+++ mount.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	'group' => array(
+		'oldskool' => array(
+			'/$user/files/dh' => array(
+				'class'=>'OC_Filestorage_FTP',
+				'options'=>array(
+					'host'=>'gb.co.id',
+					'user'=>'gb_ssowncloud',
+					'password'=>'L^fnfvPe',
+					'root'=>'oc'
+				),
+			),
+		),
+	),
+);
