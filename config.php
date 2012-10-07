4c22066c0e2d4dd65f1aeb84cecdd0a86076a970
Septian Wibisono <asep.gelo@gmail.com>
2012-10-08 01:38:44 +0700

    edited

--- config.php
+++ config.php
@@ -5,5 +5,6 @@ define("DEBUG", true);
 $CONFIG = array(
 /* Flag to indicate ownCloud is successfully installed (true = installed) */
 "installed" => false,
-"appstoreenabled" => false
+
+"appstoreenabled" => true,
 );
