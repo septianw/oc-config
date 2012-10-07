c6a61b727d7185d780027636c75c1b8dc3e24e6c
Septian Wibisono <asep.gelo@gmail.com>
2012-10-08 02:02:22 +0700

    data directory added

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ define("DEBUG", true);
 $CONFIG = array(
 /* Flag to indicate ownCloud is successfully installed (true = installed) */
 "installed" => false,
-
+"datadirectory" => "/var/www/data",
 "appstoreenabled" => true,
 );
